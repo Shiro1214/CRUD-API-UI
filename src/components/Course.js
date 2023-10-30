950566920162073a8e5b4f4508cbca5e1afff70a
shiroinhawai <henglongsun7@gmail.com>
2023-10-30 12:32:41 -1000

    Add edit grade button on each course page next to each person

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,15 +1,24 @@
 import {React, useState, useEffect} from 'react'
 import axios from 'axios'
 import { useLocation, useNavigate, Link  } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 function Course(){
     const [course, setCourse] = useState(null);
     const [loading, setLoading] = useState(true);
     const [stuIdToAdd, setStuIdToAdd] = useState('');
+    const [studIdGrade, setStudIdGrade] = useState('');
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const id = searchParams.get("id");
     const navigate = useNavigate();
+    const [show, setShow] = useState(false);
+    const [grade,setGrade] = useState('');
+    const handleClose = () => {setShow(false); setStudIdGrade(''); setGrade('');};
+    const handleShow = () => setShow(true);
     const addStudent = async () => {
         try {
             axios.put(`http://localhost:5263/api/CoursePerson/AddCoursePerson?stuId=${stuIdToAdd}&courseId=${id}`);
@@ -23,6 +32,14 @@ function Course(){
         } catch (error) {
             console.error('Error fetching data:', error);
         }
+    }
+    const modifyGrade = async () => {
+        try {
+            axios.put(`http://localhost:5263/api/CoursePerson/ChangeStudentGrade?sid=${studIdGrade}&cid=${course.id}&grade=${grade}`);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+
     }
     const deleteCourse = async () => {
         try {
@@ -68,14 +85,38 @@ function Course(){
                                     <th scope="col"></th>
                                 </tr>
                             </thead>
+                            <Modal show={show} onHide={handleClose} size="sm" aria-labelledby="contained-modal-title-vcenter" centered>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Grade Selection</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <DropdownButton id="dropdown-basic-button" title={"Grade : " + grade}  onSelect={(g)=>setGrade(g)}>
+                                    <Dropdown.Item eventKey='A'>A</Dropdown.Item>
+                                    <Dropdown.Item eventKey='B'>B</Dropdown.Item>
+                                    <Dropdown.Item eventKey='C'>C</Dropdown.Item>
+                                    <Dropdown.Item eventKey='D'>D</Dropdown.Item>
+                                    <Dropdown.Item eventKey='E'>E</Dropdown.Item>
+                                    <Dropdown.Item eventKey='F'>F</Dropdown.Item>
+                                </DropdownButton>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                Close
+                                </Button>
+                                <Button variant="primary" onClick={() => {modifyGrade(); handleClose();}}>
+                                Update Grade
+                                </Button>
+                            </Modal.Footer>
+                            </Modal>
                             <tbody>
                                 {course.coursePersons.map((cp) => (
                                     <tr key={cp.person.id}>
                                         <td>{cp.person.id}</td>
                                         <td><Link to ={`/person?id=${cp.person.id}`}>{cp.person.firstMidName} {cp.person.lastName}</Link></td>
                                         <td>{cp.person.personType}</td>
-                                        <td>{cp.gradeLetter}</td>
+                                        <td>{cp.gradeLetter} <button type = "button" className='btn btn-info' onClick={() => {setStudIdGrade(cp.person.id); handleShow();}} >Edit</button></td>
                                         <td><button type = "button" className='btn btn-danger' onClick={() => removePerson(cp.person.id)}>Remove Person</button></td>
+                                     
                                     </tr>
                                 ))}
                             </tbody>
@@ -91,8 +132,8 @@ function Course(){
                             onChange={(e) => setStuIdToAdd(e.target.value)}
                             />
                             </div>
-                            <div class="col-auto">
-                            <button type = "button" onClick={ addStudent} class="btn btn-primary mb-3">Add</button>
+                            <div className="col-auto">
+                            <button type = "button" onClick={ addStudent} className="btn btn-primary mb-3">Add</button>
                             </div>
                         </div>
                         </div>
